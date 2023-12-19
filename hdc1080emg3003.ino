a3260b53939e52e5a729a21185e2cc42f969d418
Kajidataonline <57325497+ismailsakdo@users.noreply.github.com>
2023-12-19 12:43:15 +0800

    Create hdc1080emg3003.ino

---
+++ hdc1080emg3003.ino
@@ -0,0 +1,45 @@
+//Board selection and libraries
+#include <Arduino.h>
+#if defined(ESP32)
+  #include <WiFi.h>
+#elif defined(ESP8266)
+  #include <ESP8266WiFi.h>
+#endif
+
+//Insert Library (if any)
+#include "ClosedCube_HDC1080.h"
+
+//Create instances for using library
+ClosedCube_HDC1080 hdc1080;
+
+void setup() {
+  Serial.begin(115200);  // Initialize serial
+  Wire.begin();
+  hdc1080.begin(0x40);
+
+  Serial.print("Manufacturer ID=0x");
+  Serial.println(hdc1080.readManufacturerId(), HEX); // 0x5449 ID of Texas Instruments
+  Serial.print("Device ID=0x");
+  Serial.println(hdc1080.readDeviceId(), HEX); // 0x1050 ID of the device 
+}
+
+void loop() {
+
+  // change the values
+  float nt = hdc1080.readTemperature();
+  float nh = hdc1080.readHumidity();
+  if (isnan(nt) || isnan(nh))
+
+  {
+    Serial.println("Failed to read from HTU21 sensor!");
+    return;
+  }
+
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" degrees Celsius, Humidity (%): ");
+  Serial.print(h);
+  Serial.println("Sending data to Thingspeak");
+
+  delay(1000); // Wait 15 seconds
+}
